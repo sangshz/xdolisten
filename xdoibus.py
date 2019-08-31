c579398eed9c9c8c5e46358622830e15bbad75d0
H.Z. Sang <33886976+sangshz@users.noreply.github.com>
2019-08-31 10:29:14 +0800

    Update xdoibus.py

--- xdoibus.py
+++ xdoibus.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
+import warnings
+warnings.filterwarnings("ignore")
+#from gi import require_version as gi_require_version
+#gi_require_version('IBus', '1.0')
+
 from gi.repository.IBus import Bus
 print Bus().current_input_context()
